b20ea483623882235edcf9b6bc2fa063d194b25e
jnrpobee <jnr.pobee@gmail.com>
2020-02-04 20:50:43 +0000

    file update

---
+++ webpage/index.php
@@ -0,0 +1,26 @@
+<?php include "modules/header.php"; ?>
+        <div class="container mt-3">
+
+            <div class="row">
+                <div class="col col-md-6">
+                <p class="text-capitalize">My name is Solomon B. Pobee. I come from Accra, Ghana. I am majoring web design and development with emphasis in web development.</p>
+                <p>I served my mission in Ivory Coast and as part of my mission I was to teach in French. This came me the opportunity to learn french in the <span class="text-danger">Missionary Training Center </span>(MTC) for 6 weeks. </p>
+</div>
+<div class="col col-md-6">
+                <p>I was working as an instructor in the MTC for two years until my contract ended on the 31st December 2019. As part of my role in the MTC, I was training missionaries to receive basic knowledge to get them prepared for the mission field. Also, I trained missionaries learning a new language.</p>
+                <p>I love going on trips and visiting some tourist attractive areas. I like hanging out with friends and going for swimming as well.</p>
+</div>
+            </div>
+        </div>
+        <div>
+
+        </div>
+
+
+    </body>
+    <footer>
+
+    </footer>
+
+
+</html>
