efaac995ce5fc18f96e3b639e44ba62d0a1cba37
jnrpobee <jnr.pobee@gmail.com>
2020-02-05 17:53:41 +0000

    update files

---
+++ web/webpage/assignments.php
@@ -0,0 +1,24 @@
+<?php include "modules/header.php"; ?>
+        <div class="container mt-3">
+
+            <div class="row">
+                <div class="col col-md-6">
+                <p class="text-capitalize">Coming Soon </p>
+</div>
+<div class="col col-md-6">
+                <p>.</p>
+</div>
+            </div>
+        </div>
+        <div>
+
+        </div>
+
+
+    </body>
+    <footer>
+
+    </footer>
+
+
+</html>
---
+++ web/webpage/modules/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <script src="js/myScript.js" type="text/javascript"></script>
+        <script src="js/jQuery.js" type="text/javascript"></script>
+        <Script src="bootstrap/js/bootstrap.bundle.min.js"></Script>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" >
+    </head>
+
+    <body>
+        <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Solomon B. Pobee</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="../webpage/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../webpage/assignments.php">Assignments</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+        </header>
